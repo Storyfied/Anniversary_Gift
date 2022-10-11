ae4d1c8a4e14df8d7a32f808f92943c98c6e76bc
Storyfied <tan.weixinn@gmail.com>
2022-10-12 01:23:16 +0800

    - Initial commit - Implemented a scrolling background - Imported a font and custom made background for the scroller

---
+++ Project/Assets/Scripts/UI/UIScroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScroller : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private float m_horizontalScrollSpeed = 0f;
+    [SerializeField] private float m_verticalScrollSpeed = 0f;
+
+    private RawImage m_rawImage = null;
+
+    private void Awake()
+    {
+        m_rawImage = GetComponent<RawImage>();
+    }
+
+    private void Update()
+    {
+        m_rawImage.uvRect = new Rect(m_rawImage.uvRect.position + new Vector2(m_horizontalScrollSpeed, m_verticalScrollSpeed) * Time.deltaTime, m_rawImage.uvRect.size);
+    }
+}
